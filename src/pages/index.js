c394e6abd58946603b7ac7ad006ba52f859a5da3
nicolas lequeux <contact@nicolaslequeux.com>
2020-01-27 16:35:05 +0100

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,69 @@
 import React from "react"
+import { Row, Col } from 'reactstrap'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
+
+import Post from "../components/Post"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Home" keywords={['Gatsby', 'Application', 'React']}/>
     <h1>Home page</h1>
+    <Row>
+      <Col md="8">
+        <StaticQuery query={indexQuery} render={data => {
+          return (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Post title={node.frontmatter.title}
+                author={node.frontmatter.author} 
+                path={node.frontmatter.path}
+                date={node.frontmatter.date}
+                fluid={node.frontmatter.image.childImageSharp.fluid}             
+                body={node.excerpt}
+                />
+              ))}
+            </div>
+          )
+        }} />
+      </Col>
+    <Col md="4">
+      <div style={{width: "100%", height: "100%", backgroundColor: "grey"}}></div>
+    </Col>      
+    </Row>
   </Layout>
 )
 
+const indexQuery = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMM Do YYY")
+            author
+            path
+            image {
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+
+
 export default IndexPage
+
+
---
+++ src/pages/posts/002-second-post.md
@@ -0,0 +1,11 @@
+---
+title: "Second Post - HTMH"
+date: 2018-12-01 18:00:00
+author: "Nicolas Lequeux"
+path: "/second-post"
+image: "../../images/venise-2.jpg"
+---
+
+Today, we are gonna talk about HTML5 with Gaëlle.
+
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
